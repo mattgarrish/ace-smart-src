2a985ff68fcf3e63e53ff2b257756317419bb5ac
Matt Garrish <matt.garrish@gmail.com>
2018-01-12 09:43:46 -0500

    organize initialization code

--- js/init.js
+++ js/init.js
@@ -1,12 +1,37 @@
 
 	'use strict';
 	
+	/*
+	 *  REPORTING
+	 */
+	
+	// add reporting fields to the conformance success criteria
+	window.onload = smartReport.addSuccessCriteriaReporting();
+	
+	
+	
+	/* 
+	 * ERROR HANDLING
+	 */
+	 
+	// initialize error reporting
+	smartError.init();
+	
+	
+	
+	/* 
+	 * POLYFILLS
+	 */
+	
 	// initialize details polyfill
 	$('details').details();
 	
-	// set error elements
-	smartError.init();
 	
+	
+	/* 
+	 * DIALOG CONFIGURATION
+	 */
+
 	// initialize dialogs
 	var import_dialog = $("#import").dialog({
 		autoOpen: false,
@@ -37,65 +62,13 @@
 	});
 	
 	
-	/* generate the reporting fields when page loads */
-	window.onload = smartReport.addSuccessCriteriaReporting();
 	
-	/* watch for local load */
-	$('#local-load input[type="button"]').click( function(){
-		smartManage.loadLocalReport();
-	});
+	/* 
+	 * EVENT HANDLING
+	 */
 	
 	
-	/* watch for EPUB format changes */
-	$('input[name="epub-format"]').click( function(){
-		smartFormat.setEPUBVersion(this.value);
-	});
-	
-	/* watch for conformance level changes */
-	$('input[name="wcag-level"]').click( function(){
-		smartConformance.setWCAGConformanceLevel(this.value);
-	});
-	
-	/* watch for optional criteria display changes */
-	$('input.optional-criteria').click( function(){
-		smartConformance.displaySuccessCriteria({wcag_level: this.id.replace('show-',''), display: (this.checked ? true : false)});
-	});
-	
-	/* watch for clicks on SC status radio buttons */
-	$('input.sc_status').click( function(){
-		smartConformance.setSCStatus({name: this.name, value: this.value});
-	});
-	
-	/* watch for clicks on SC filter */
-	$('input.hide_sc').click( function(){
-		smartConformance.filterSCByStatus(this);
-	});
-	
-	/* watch for clicks on SC filter */
-	$('input[name="status"]').click( function(){
-		smartConformance.setGlobalSCStatus(this.value);
-	});
-	
-	/* watch for clicks on SC filter */
-	$('input.excl-test').click( function(){
-		smartConformance.configureContentTypeTests({type: this.id, exclude: this.checked});
-	});
-	
-	/* watch for clicks to show/hide sc bodies */
-	$('input[name="sc-body"]').click( function(){
-		smartConformance.showSCBody(this.value == 'true' ? true : false);
-	});
-	
-	/* watch for clicks to show/hide help links */
-	$('input[name="link-exp"]').click( function(){
-		smartConformance.showSCHelpLinks(this.value == 'true' ? true : false);
-	});
-	
-	/* watch for clicks to show/hide help links */
-	$('input.show-note').click( function(){
-		smartConformance.showSCNoteField(this);
-	});
-
+	/* INTERFACE */
 	
 	/* watch for save button click */
 	$('#save-button').click( function(){
@@ -114,13 +87,77 @@
 		smartError.hideErrorPane();
 	});
 	
+	
+	/* START TAB */
+	
+	/* watch for local load of a report */
+	$('#local-load input[type="button"]').click( function(){
+		smartManage.loadLocalReport();
+	});
+	
+	/* watch for EPUB format changes */
+	$('input[name="epub-format"]').click( function(){
+		smartFormat.setEPUBVersion(this.value);
+	});
+	
+	
+	/* CONFORMANCE TAB */
+	
+	/* watch for wcag conformance level changes */
+	$('input[name="wcag-level"]').click( function(){
+		smartConformance.setWCAGConformanceLevel(this.value);
+	});
+	
+	/* watch for optional criteria display changes */
+	$('input.optional-criteria').click( function(){
+		smartConformance.displaySuccessCriteria({wcag_level: this.id.replace('show-',''), display: (this.checked ? true : false)});
+	});
+	
+	/* watch for filtering of success criteria by status */
+	$('input.hide_sc').click( function(){
+		smartConformance.filterSCByStatus(this);
+	});
+	
+	/* watch for setting of global status to all success criteria */
+	$('input[name="status"]').click( function(){
+		smartConformance.setGlobalSCStatus(this.value);
+	});
+	
+	/* watch for filtering of success criteria by content type */
+	$('input.excl-test').click( function(){
+		smartConformance.configureContentTypeTests({type: this.id, exclude: this.checked});
+	});
+	
+	/* watch for clicks to show/hide success criteria descriptions */
+	$('input[name="sc-body"]').click( function(){
+		smartConformance.showSCBody(this.value == 'true' ? true : false);
+	});
+	
+	/* watch for clicks to expand/collapse help links */
+	$('input[name="link-exp"]').click( function(){
+		smartConformance.showSCHelpLinks(this.value == 'true' ? true : false);
+	});
+	
+	/* watch for changes to success criteria status radio buttons */
+	$('input.sc_status').click( function(){
+		smartConformance.setSCStatus({name: this.name, value: this.value});
+	});
+	
+	/* watch for clicks to show/hide success criteria note fields */
+	$('input.show-note').click( function(){
+		smartConformance.showSCNoteField(this);
+	});
+
+	
+	/* DISCOVERY TAB */
+	
 	/* watch for click to add custom accessibilityFeature */
 	$('#add-a11y-feature').click( function(){
 		smartDiscovery.addCustomFeature();
 		return false;
 	});
 	
-	/* watch for click to add custom accessibilityFeature */
+	/* watch for click to add additional accessModeSufficient sets */
 	$('#add-sufficient').click( function(){
 		smartDiscovery.addNewSufficientSet();
 		return false;
@@ -130,11 +167,17 @@
 	$('#discovery_button').click( function(){
 		smartDiscovery.generateDiscoveryMetadata();
 	});
-		
+	
+	
+	/* CERTIFICATION TAB */
+	
 	/* watch for click on button to generate certification metadata */
 	$('#generate-certification-metadata').click( function(){
 		smartCertification.generateCertificationMetadata();
 	});
+	
+	
+	/* REPORTING TAB */
 	
 	/* watch for changes to note output */
 	$('input[name="show-notes"]').click( function(){
@@ -150,3 +193,5 @@
 	$('#generate-report').click( function(){
 		smartReport.generateConformanceReport(); 
 	});
+
+
