85f5b569f4baa75a2825f6e7abd744d8a78fa081
MattGarrish <mattgarrish@users.noreply.github.com>
2018-05-21 08:26:12 -0400

    fix missing onix id padding

--- js/manage.js
+++ js/manage.js
@@ -120,7 +120,8 @@ var smartManage = (function() {
 			reportJSON.distribution.onix = {};
 			reportJSON.distribution.onix['00'] = document.getElementById('onix00').value.trim();
 			for (var o = 10; o < 25; o++) {
-				var onix_chkbox = document.getElementById('onix' + (o < 10 ? '0' + String(o) : o));
+				var onix_id = o < 10 ? '0' + String(o) : o;
+				var onix_chkbox = document.getElementById('onix' + onix_id);
 				if (onix_chkbox) {
 					reportJSON.distribution.onix[onix_id] = onix_chkbox.checked;
 				}
