75dce943f0e00e6b0fee8801f19496f7176413a4
Matt Garrish <mattgarrish@users.noreply.github.com>
2022-10-21 09:25:18 -0300

    add favicon for site

--- usersc/templates/standard/header.php
+++ usersc/templates/standard/header.php
@@ -25,6 +25,10 @@ body {
 font-family: 'Roboto', sans-serif !important;
 }
 </style>
+
+<!-- favicon -->
+<link rel="icon" type="image/x-icon" href="https://smart.daisy.org/favicon.ico" sizes="any"/>
+
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/bootstrap.min.js" type="text/javascript"></script>
 <link href="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/css/hamburgers.min.css" rel="stylesheet">
