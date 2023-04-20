bf458511c709037121819273db55a33b0b3bf53e
Matt Garrish <mattgarrish@users.noreply.github.com>
2023-04-20 11:09:45 -0400

    auto-pass 4.1.1

--- php/version.php
+++ php/version.php
@@ -1,4 +1,4 @@
 
 <?php
-	$smart_version = '2023-02-24T12:00:00Z'; # used to refresh all css and js in browsers 
+	$smart_version = '2023-03-20T12:00:00Z'; # used to refresh all css and js in browsers 
 ?>
